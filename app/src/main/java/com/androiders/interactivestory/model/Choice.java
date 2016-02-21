e191d0e00a2a112471b2768e8c302b33965e7c7e
mgumiero9 <mgumiero9@gmail.com>
2016-02-21 14:46:52 -0300

    Implemented Getter and Setter methods for classes Page and Choice

--- app/src/main/java/com/androiders/interactivestory/model/Choice.java
+++ app/src/main/java/com/androiders/interactivestory/model/Choice.java
@@ -7,4 +7,20 @@ public class Choice {
 
     private String mText;
     private int mNextPage;
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
 }
